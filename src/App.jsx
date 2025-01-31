4fa872b5d803047a41a3ddb95815469c673eed0d
Jean-Philippe Ciman <jeanffy@gmail.com>
2025-01-31 09:21:46 +0100

    Add format buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,76 @@
 import { useState } from "react";
 
 function App() {
-  const [jsonResult, setJsonResult] = useState("");
+	const [files, setFiles] = useState();
+	const [jsonResult, setJsonResult] = useState("");
 
-  const handleFiles = (e) => {
-    const files = e.target.files;
-    if (!files.length) return;
+	const handleChange = (e) => {
+		const files = e.target.files;
+		if (!files.length) return;
+		setFiles(files);
+	};
 
-    const fileMap = {};
-    for (let i = 0; i < files.length; i++) {
-      const fileName = files[i].name;
-      fileMap[fileName] = `require('../../assets/${fileName}')`;
-    }
+	const rnFileMap = () => {
+		const fileMap = {};
+		for (let i = 0; i < files.length; i++) {
+			const fileName = files[i].name;
+			fileMap[fileName] = `require('../../assets/${fileName}')`;
+		}
 
-    setJsonResult(JSON.stringify(fileMap, null, 2));
-  };
+		setJsonResult(JSON.stringify(fileMap, null, 2));
+	};
 
-  const handleCopy = () => {
-    navigator.clipboard
-      .writeText(jsonResult)
-      .then(() => {
-        alert("Contenu copié dans le presse-papier !");
-      })
-      .catch((err) => {
-        alert("Erreur lors de la copie : " + err);
-      });
-  };
+	const themesBuilder = () => {
+		const fileMap = [];
+		for (let i = 0; i < files.length; i++) {
+			let fileName = files[i].name;
+			fileName = fileName.substring(3);
+			let index = fileName.lastIndexOf(".");
+			fileName = index !== -1 ? fileName.slice(0, index) : fileName;
+			const theme = {
+				themeId: i + 1,
+				label: fileName,
+			};
+			fileMap.push(theme);
+		}
+		setJsonResult(JSON.stringify(fileMap, null, 2));
+	};
 
-  return (
-    <div id="app">
-      <div id="left">
-        <input type="file" id="input-file" multiple onChange={handleFiles} />
-      </div>
-      <div id="right">
-        <button type="button" id="copy" onClick={handleCopy}>
-          Copier
-        </button>
-        <pre>{jsonResult}</pre>
-      </div>
-    </div>
-  );
+	const handleCopy = () => {
+		navigator.clipboard
+			.writeText(jsonResult)
+			.then(() => {
+				alert("Contenu copié dans le presse-papier !");
+			})
+			.catch((err) => {
+				alert("Erreur lors de la copie : " + err);
+			});
+	};
+
+	return (
+		<div id="app">
+			<div id="left">
+				<input type="file" id="input-file" multiple onChange={handleChange} />
+				<button type="button" onClick={rnFileMap}>
+					File map
+				</button>
+				<button type="button" onClick={themesBuilder}>
+					Build themes
+				</button>
+			</div>
+			<div id="right">
+				<div id="toolbar">
+					<button type="button" id="copy" onClick={handleCopy}>
+						Copier
+					</button>
+					<button type="button" id="reset" onClick={handleCopy}>
+						Reset
+					</button>
+				</div>
+				<pre>{jsonResult}</pre>
+			</div>
+		</div>
+	);
 }
 
 export default App;
